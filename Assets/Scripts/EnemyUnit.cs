e89c29419a85678cbea5a08e792f033b04c717fb
Hyde <liquid_assassin@hotmail.com>
2018-02-01 19:38:28 +0800

    Added 40% of the game

---
+++ Assets/Scripts/EnemyUnit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyUnit : MonoBehaviour {
+
+	public int hp;
+
+	void Start () 
+	{
+		GetComponent<NavMeshAgent> ().SetDestination (GameManager.instance.AIDestination.position);
+	}
+
+	public void DealDamage(int damage)
+	{
+		hp -= damage;
+		if (hp < 0)
+		{
+			GameManager.instance.UpdateScore ();
+			Destroy (gameObject); //Replace with death animation
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public Transform[] spawnPoints;
+	public int wave = 1, score = 0, cabinHP;
+	public Text scoreUI;
+	static IEnumerator spawnLoop;
+	public GameObject[] enemyUnits;
+	public Transform AIDestination;
+
+	void Awake()
+	{
+		instance = this;
+		spawnLoop = spawnMachine ();
+	}
+
+	void Update()
+	{
+		
+	}
+	
+	public void StartGame()
+	{
+		StartCoroutine(spawnMachine());
+	}
+
+	public void EndGame()
+	{
+		StopCoroutine(spawnMachine());
+	}
+
+	IEnumerator spawnMachine()
+	{
+		float enemyCount = 5;
+		for (;;)
+		{
+			yield return new WaitForSeconds(Random.Range(1, (wave / (wave + 5)) - wave));
+			int desinatedSpawn = Random.Range(0, spawnPoints.Length);
+
+			// Code needs to change for other variants
+			Instantiate(enemyUnits[0], spawnPoints[desinatedSpawn].position, spawnPoints[desinatedSpawn].rotation);
+
+			--enemyCount;
+			if (enemyCount == 0)
+			{
+				++wave;
+				enemyCount = 3 + wave * 2;
+			}
+		}
+	}
+
+	public void UpdateScore()
+	{
+		++score;
+		scoreUI.text = "Score: " + score;
+	}
+}
