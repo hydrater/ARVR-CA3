b9edb62a8700e0189ed60aaf11a39cb045ee0e2c
LanJay <leejedmc@gmail.com>
2018-02-02 00:02:38 +0800

    Gun Script + Particles

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public GameObject sandHitEffect;
+    public GameObject[] fleshHitEffects;
+    public GameObject woodHitEffect;
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+
+    public Camera Cam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        OVRInput.Update(); // Call before checking the input
+
+        if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) == 1f && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            EnemyUnit target = hit.transform.root.GetComponent<EnemyUnit>();
+            if (target != null)
+            {
+                target.DealDamage(10);
+            }
+
+            if (hit.collider.sharedMaterial != null)
+            {
+                string materialName = hit.collider.sharedMaterial.name;
+
+                switch (materialName)
+                {
+                    case "Sand":
+                        SpawnDecal(hit, sandHitEffect);
+                        break;         
+                    case "Wood":
+                        SpawnDecal(hit, woodHitEffect);
+                        break;
+                    case "Meat":
+                        SpawnDecal(hit, fleshHitEffects[Random.Range(0, fleshHitEffects.Length)]);
+                        break;
+                    case "Character":
+                        SpawnDecal(hit, fleshHitEffects[Random.Range(0, fleshHitEffects.Length)]);
+                        break;
+                }
+            }
+        }
+    }
+
+    void SpawnDecal(RaycastHit hit, GameObject prefab)
+    {
+        GameObject spawnedDecal = GameObject.Instantiate(prefab, hit.point, Quaternion.LookRotation(hit.normal));
+        spawnedDecal.transform.SetParent(hit.collider.transform);
+        Destroy(spawnedDecal, 2f);
+    }
+}
