6a39709b34744f6c430b12da136da06e5ab014c6
LanJay <leejedmc@gmail.com>
2018-02-05 00:04:14 +0800

    Shooting with muzzle Flash

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public GameObject sandHitEffect;
+    public GameObject[] fleshHitEffects;
+    public GameObject woodHitEffect;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            //Insert Damage here
+
+            //Insert fleshHitEffects here
+            Destroy(col.gameObject);
+        }
+    }
+}
--- Assets/VRTK/Examples/ExampleResources/Scripts/RealGun.cs
+++ Assets/VRTK/Examples/ExampleResources/Scripts/RealGun.cs
@@ -4,23 +4,31 @@
 
     public class RealGun : VRTK_InteractableObject
     {
+
+
+        //public float damage = 10f;
+        //public float range = 100f;
+        //public float fireRate = 15f;
+
+        public ParticleSystem muzzleFlash;
+
         public float bulletSpeed = 200f;
         public float bulletLife = 5f;
 
-        private GameObject bullet;
-        private GameObject trigger;
-        private RealGun_Slide slide;
-        private RealGun_SafetySwitch safetySwitch;
+        public GameObject bullet;
+        //private GameObject trigger;
+        //private RealGun_Slide slide;
+        //private RealGun_SafetySwitch safetySwitch;
 
-        private Rigidbody slideRigidbody;
-        private Collider slideCollider;
-        private Rigidbody safetySwitchRigidbody;
-        private Collider safetySwitchCollider;
+        //private Rigidbody slideRigidbody;
+        //private Collider slideCollider;
+        //private Rigidbody safetySwitchRigidbody;
+        //private Collider safetySwitchCollider;
 
         private VRTK_ControllerEvents controllerEvents;
 
-        private float minTriggerRotation = -10f;
-        private float maxTriggerRotation = 45f;
+        //private float minTriggerRotation = -10f;
+        //private float maxTriggerRotation = 45f;
 
         private void ToggleCollision(Rigidbody objRB, Collider objCol, bool state)
         {
@@ -28,25 +36,25 @@
             objCol.isTrigger = state;
         }
 
-        private void ToggleSlide(bool state)
-        {
-            if (!state)
-            {
-                slide.ForceStopInteracting();
-            }
-            slide.enabled = state;
-            slide.isGrabbable = state;
-            ToggleCollision(slideRigidbody, slideCollider, state);
-        }
+        //private void ToggleSlide(bool state)
+        //{
+        //    if (!state)
+        //    {
+        //        slide.ForceStopInteracting();
+        //    }
+        //    slide.enabled = state;
+        //    slide.isGrabbable = state;
+        //    ToggleCollision(slideRigidbody, slideCollider, state);
+        //}
 
-        private void ToggleSafetySwitch(bool state)
-        {
-            if (!state)
-            {
-                safetySwitch.ForceStopInteracting();
-            }
-            ToggleCollision(safetySwitchRigidbody, safetySwitchCollider, state);
-        }
+        //private void ToggleSafetySwitch(bool state)
+        //{
+        //    if (!state)
+        //    {
+        //        safetySwitch.ForceStopInteracting();
+        //    }
+        //    ToggleCollision(safetySwitchRigidbody, safetySwitchCollider, state);
+        //}
 
         public override void Grabbed(VRTK_InteractGrab currentGrabbingObject)
         {
@@ -54,23 +62,23 @@
 
             controllerEvents = currentGrabbingObject.GetComponent<VRTK_ControllerEvents>();
 
-            ToggleSlide(true);
-            ToggleSafetySwitch(true);
+            //ToggleSlide(true);
+            //ToggleSafetySwitch(true);
 
             //Limit hands grabbing when picked up
             if (VRTK_DeviceFinder.GetControllerHand(currentGrabbingObject.controllerEvents.gameObject) == SDK_BaseController.ControllerHand.Left)
             {
                 allowedTouchControllers = AllowedController.LeftOnly;
                 allowedUseControllers = AllowedController.LeftOnly;
-                slide.allowedGrabControllers = AllowedController.RightOnly;
-                safetySwitch.allowedGrabControllers = AllowedController.RightOnly;
+                //slide.allowedGrabControllers = AllowedController.RightOnly;
+                //safetySwitch.allowedGrabControllers = AllowedController.RightOnly;
             }
             else if (VRTK_DeviceFinder.GetControllerHand(currentGrabbingObject.controllerEvents.gameObject) == SDK_BaseController.ControllerHand.Right)
             {
                 allowedTouchControllers = AllowedController.RightOnly;
                 allowedUseControllers = AllowedController.RightOnly;
-                slide.allowedGrabControllers = AllowedController.LeftOnly;
-                safetySwitch.allowedGrabControllers = AllowedController.LeftOnly;
+                //slide.allowedGrabControllers = AllowedController.LeftOnly;
+                //safetySwitch.allowedGrabControllers = AllowedController.LeftOnly;
             }
         }
 
@@ -78,14 +86,14 @@
         {
             base.Ungrabbed(previousGrabbingObject);
 
-            ToggleSlide(false);
-            ToggleSafetySwitch(false);
+            //ToggleSlide(false);
+            //ToggleSafetySwitch(false);
 
             //Unlimit hands
             allowedTouchControllers = AllowedController.Both;
             allowedUseControllers = AllowedController.Both;
-            slide.allowedGrabControllers = AllowedController.Both;
-            safetySwitch.allowedGrabControllers = AllowedController.Both;
+            //slide.allowedGrabControllers = AllowedController.Both;
+            //safetySwitch.allowedGrabControllers = AllowedController.Both;
 
             controllerEvents = null;
         }
@@ -93,56 +101,88 @@
         public override void StartUsing(VRTK_InteractUse currentUsingObject)
         {
             base.StartUsing(currentUsingObject);
-            if (safetySwitch.safetyOff)
-            {
-                slide.Fire();
+            //if (safetySwitch.safetyOff)
+            //{
+                //slide.Fire();
                 FireBullet();
+                Debug.Log("Bullet Shot");
                 VRTK_ControllerHaptics.TriggerHapticPulse(VRTK_ControllerReference.GetControllerReference(controllerEvents.gameObject), 0.63f, 0.2f, 0.01f);
-            }
-            else
-            {
-                VRTK_ControllerHaptics.TriggerHapticPulse(VRTK_ControllerReference.GetControllerReference(controllerEvents.gameObject), 0.08f, 0.1f, 0.01f);
-            }
+
+            //EnemyUnit target = hit.transform.root.GetComponent<EnemyUnit>();
+            //if (target != null)
+            //{
+            //    target.DealDamage(10);
+            //}
+
+            //if (hit.collider.sharedMaterial != null)
+            //{
+            //    string materialName = hit.collider.sharedMaterial.name;
+
+            //    switch (materialName)
+            //    {
+            //        case "Sand":
+            //            SpawnDecal(hit, sandHitEffect);
+            //            break;
+            //        case "Wood":
+            //            SpawnDecal(hit, woodHitEffect);
+            //            break;
+            //        case "Meat":
+            //            SpawnDecal(hit, fleshHitEffects[Random.Range(0, fleshHitEffects.Length)]);
+            //            break;
+            //        case "Character":
+            //            SpawnDecal(hit, fleshHitEffects[Random.Range(0, fleshHitEffects.Length)]);
+            //            break;
+            //    }
+            //}
+            //}
+            //else
+            //{
+            //    VRTK_ControllerHaptics.TriggerHapticPulse(VRTK_ControllerReference.GetControllerReference(controllerEvents.gameObject), 0.08f, 0.1f, 0.01f);
+            //}
         }
 
         protected override void Awake()
         {
             base.Awake();
-            bullet = transform.Find("Bullet").gameObject;
+            //bullet = transform.Find("Bullet").gameObject;
             bullet.SetActive(false);
 
-            trigger = transform.Find("TriggerHolder").gameObject;
+            //trigger = transform.Find("TriggerHolder").gameObject;
 
-            slide = transform.Find("Slide").GetComponent<RealGun_Slide>();
-            slideRigidbody = slide.GetComponent<Rigidbody>();
-            slideCollider = slide.GetComponent<Collider>();
+            //slide = transform.Find("Slide").GetComponent<RealGun_Slide>();
+            //slideRigidbody = slide.GetComponent<Rigidbody>();
+            //slideCollider = slide.GetComponent<Collider>();
 
-            safetySwitch = transform.Find("SafetySwitch").GetComponent<RealGun_SafetySwitch>();
-            safetySwitchRigidbody = safetySwitch.GetComponent<Rigidbody>();
-            safetySwitchCollider = safetySwitch.GetComponent<Collider>();
+            //safetySwitch = transform.Find("SafetySwitch").GetComponent<RealGun_SafetySwitch>();
+            //safetySwitchRigidbody = safetySwitch.GetComponent<Rigidbody>();
+            //safetySwitchCollider = safetySwitch.GetComponent<Collider>();
         }
 
         protected override void Update()
         {
             base.Update();
-            if (controllerEvents)
-            {
-                var pressure = (maxTriggerRotation * controllerEvents.GetTriggerAxis()) - minTriggerRotation;
-                trigger.transform.localEulerAngles = new Vector3(0f, pressure, 0f);
-            }
-            else
-            {
-                trigger.transform.localEulerAngles = new Vector3(0f, minTriggerRotation, 0f);
-            }
+            //if (controllerEvents)
+            //{
+            //    var pressure = (maxTriggerRotation * controllerEvents.GetTriggerAxis()) - minTriggerRotation;
+            //    trigger.transform.localEulerAngles = new Vector3(0f, pressure, 0f);
+            //}
+            //else
+            //{
+            //    trigger.transform.localEulerAngles = new Vector3(0f, minTriggerRotation, 0f);
+            //}
         }
 
         private void FireBullet()
         {
+            muzzleFlash.Play();
+
             GameObject bulletClone = Instantiate(bullet, bullet.transform.position, bullet.transform.rotation) as GameObject;
             bulletClone.SetActive(true);
             Rigidbody rb = bulletClone.GetComponent<Rigidbody>();
             rb.AddForce(bullet.transform.forward * bulletSpeed);
             Destroy(bulletClone, bulletLife);
+
+
         }
     }
 }
