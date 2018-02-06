6ada1e9bbf2769cc19d7e2b93ed71584af93c78d
Daraxxus <sandvichisawesome@gmail.com>
2018-02-06 17:18:40 +0800

    Menu

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+    public GameObject Panel;
+    public GameObject HTPPanel;
+	// Use this for initialization
+	void Start () {
+        Panel = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void QuitButton ()
+    {
+        Application.Quit();
+    }
+
+    public void StartButton()
+    {
+        Panel.SetActive(false);
+    }
+
+    public void BackButton()
+    {
+        HTPPanel.SetActive(false);
+    }
+
+    public void HowToPlay()
+    {
+        HTPPanel.SetActive(true);
+    }
+}
